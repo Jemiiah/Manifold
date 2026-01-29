4df7609a8f4c7654daeb4c491c8dd54e7d0d9608
Jemiiah <seunjeremiah2003@gmail.com>
2026-01-29 09:41:10 -0800

    feat: improved frontend UI

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
             />
 
             {/* Featured Market */}
-            <FeaturedMarket market={markets[0]} onClick={selectMarket} />
+            <FeaturedMarket markets={markets} onClick={selectMarket} />
 
             {/* Market Grid */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
